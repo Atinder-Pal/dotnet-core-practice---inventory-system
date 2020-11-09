d8e13238b9acaf8b1d8a8e32d76791bd7ef90c85
Atinder-Pal <atty.pal46@gmail.com>
2020-11-09 15:04:23 -0700

    Create Folder Structure, Create Product Model, Add Migration to seed data

---
+++ InventorySystemDay1/InventorySystemDay1/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventorySystemDay1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventorySystemDay1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public Product CreateProduct(string name, string quantity, string discontinued)
+        {
+
+            return null;
+        }
+     }
+}
---
+++ InventorySystemDay1/InventorySystemDay1/Migrations/20201109215142_SeedTable.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventorySystemDay1.Migrations
+{
+    public partial class SeedTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "ID", "Discontinued", "Name", "Quantity" },
+                values: new object[,]
+                {
+                    { -1, true, "Chopping Board", 3 },
+                    { -2, false, "Knife", 20 },
+                    { -3, false, "Bowl", 100 },
+                    { -4, false, "Plates", 3 },
+                    { -5, false, "IPhone", 200 },
+                    { -6, false, "IPad", 500 },
+                    { -7, false, "Heater", 50 },
+                    { -8, false, "Fan", 200 },
+                    { -9, false, "Water Bottle", 200 },
+                    { -10, false, "Chair", 200 },
+                    { -11, true, "Pen", 200 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -11);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -10);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -9);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -8);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -7);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -6);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -5);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -4);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -3);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -2);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "ID",
+                keyValue: -1);
+        }
+    }
+}
